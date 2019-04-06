2fb9b59cad96e11a67fb22f58bdefba88ee15a7e
Richard Joseph <josephri@oregonstate.edu>
2019-04-05 23:13:17 -0400

    Initial Commit

---
+++ Langston's Ant/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Langston's Ant
+//
+//  Created by Richard Joseph on 4/5/19.
+//  Copyright © 2019 Richard Joseph. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
